32ea7c5f566a9fc098f688e9717976f36ba1d222
임원정 <oddish0513@gmail.com>
2025-08-12 18:28:26 +0200

    fix: disable the spring security default login

--- backend/matechai/src/main/java/com/ft/matechai/MatechaiApplication.java
+++ backend/matechai/src/main/java/com/ft/matechai/MatechaiApplication.java
@@ -2,8 +2,9 @@ package com.ft.matechai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MatechaiApplication {
 
     public static void main(String[] args) {
