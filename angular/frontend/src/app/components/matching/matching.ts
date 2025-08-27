dd5409d30e28f3c200280c792e63e91f8f003290
Ahsan Abdul Salam <ahsanbaloch899@gmail.com>
2025-08-27 15:13:06 +0200

    created frontend

---
+++ angular/frontend/src/app/components/matching/matching.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-matching',
+  imports: [],
+  templateUrl: './matching.html',
+  styleUrl: './matching.scss'
+})
+export class Matching {
+
+}
