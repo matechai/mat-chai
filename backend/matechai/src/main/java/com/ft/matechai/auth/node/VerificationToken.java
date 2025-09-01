be4e07311d2cff1277e55b0648b6219171b72bb1
임원정 <oddish0513@gmail.com>
2025-09-01 22:19:31 +0200

    chore: add constructors

--- backend/matechai/src/main/java/com/ft/matechai/auth/node/VerificationToken.java
+++ backend/matechai/src/main/java/com/ft/matechai/auth/node/VerificationToken.java
@@ -1,6 +1,8 @@
 package com.ft.matechai.auth.node;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
@@ -8,6 +10,8 @@ import org.springframework.data.neo4j.core.schema.Node;
 @Getter
 @Setter
 @Node
+@NoArgsConstructor
+@AllArgsConstructor
 public class VerificationToken {
 
     @Id
--- backend/matechai/src/main/java/com/ft/matechai/user/node/UserNode.java
+++ backend/matechai/src/main/java/com/ft/matechai/user/node/UserNode.java
@@ -1,12 +1,16 @@
 package com.ft.matechai.user.node;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.neo4j.core.schema.*;
 
 @Node("User")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserNode {
     @Id @GeneratedValue
     private Long id;
