22d04e3ac9234c6babb6d9ecdabe7f59d03ea9c5
임원정 <oddish0513@gmail.com>
2025-09-03 20:44:31 +0200

    feat: create log in dtos #12

---
+++ backend/matechai/src/main/java/com/ft/matechai/auth/dto/LoginResponseDTO.java
@@ -0,0 +1,19 @@
+package com.ft.matechai.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDTO {
+
+    private String token;
+    private String tokenType;
+    private boolean firstLogin;
+
+}
