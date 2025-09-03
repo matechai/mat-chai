c65a712bfc2447344c7e9b5a24c9101caca20dde
임원정 <oddish0513@gmail.com>
2025-09-03 20:45:29 +0200

    feat: add exception handlers #12

--- backend/matechai/src/main/java/com/ft/matechai/user/repository/UserRepository.java
+++ backend/matechai/src/main/java/com/ft/matechai/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.ft.matechai.user.repository;
 
+import com.ft.matechai.exception.AuthExceptions;
 import com.ft.matechai.user.node.UserNode;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
@@ -10,10 +11,18 @@ import java.util.Optional;
 
 public interface UserRepository extends Neo4jRepository<UserNode, Long> {
 
-    Optional<UserNode> findByUsername(String username);
-
     @Query("MATCH (a:User {username: $username}) Return a")
-    Optional<UserNode> findByUsernameCustom(@Param("username") String username);
+    Optional<UserNode> findByUsername(@Param("username") String username);
+
+    default UserNode findByUsernameOrThrow(String username) {
+        return findByUsername(username)
+                .orElseThrow(() -> new AuthExceptions.UnauthorizedException("Invalid username"));
+    }
+
+    default UserNode findByIdOrThrow(Long id) {
+        return findById(id)
+                .orElseThrow(() -> new AuthExceptions.UnauthorizedException("Invalid id"));
+    }
 
     @Query("MATCH (a:User) RETURN a")
     List<UserNode> findAllUsers();
