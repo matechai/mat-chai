a9fe85263fb9ed94b3cadb3dbcce3b07161691d7
Lais Kurdy <lais@Laiss-Mac.local>
2025-09-08 14:31:06 +0200

    service layer planning

--- backend/matechai/src/main/java/com/ft/matechai/database/UserService.java
+++ backend/matechai/src/main/java/com/ft/matechai/database/UserService.java
@@ -10,15 +10,47 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User addUser(User u) {
+    // GETTERS
+
+    @Transactional(readOnly = true)
+    public Optional<User> getUserById(Long userId) {
+        return userRepository.findById(userId);
+    }
+
+    // SETTERS
+
+    @Transactional
+    public User saveUser(User u) {
         return userRepository.save(u);
     }
 
-    public void removeUser(Long id) {
+    @Transactional
+    public void removeUser(Long userId) {
         userRepository.deleteById(id);
     }
 
-    public void followUser(Long followerId, Long followedId) {
+    @Transactional
+    public void followUser(Long userId, Long toFollowId) {
+        //LK Add code: Call a function to find follower user and followed user then add the followed user to the follower's Set and save it all
+    }
+
+    @Transactional
+    public void unfollowUser(Long userId, Long toUnfollowId) {
+        //LK Add code: Call a function to find follower user and followed user then add the followed user to the follower's Set and save it all
+    }
+
+    @Transactional
+    public void viewUser(Long userId, Long toViewId) {
+        //LK Add code: Call a function to find follower user and followed user then add the followed user to the follower's Set and save it all
+    }
+
+    @Transactional
+    public void unviewUser(Long userId, Long toUnviewId) {
+        //LK Add code: Call a function to find follower user and followed user then add the followed user to the follower's Set and save it all
+    }
+
+    @Transactional
+    public void blockUser(Long userId, Long toBlockId) {
         //LK Add code: Call a function to find follower user and followed user then add the followed user to the follower's Set and save it all
     }
 }
