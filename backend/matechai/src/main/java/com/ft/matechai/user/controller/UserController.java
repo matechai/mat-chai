7d0c1c5f570058121be37847db08bf59ac8491fb
임원정 <oddish0513@gmail.com>
2025-09-19 21:22:13 +0200

    refactor: change api address

--- backend/matechai/src/main/java/com/ft/matechai/user/controller/UserController.java
+++ backend/matechai/src/main/java/com/ft/matechai/user/controller/UserController.java
@@ -5,7 +5,7 @@ import com.ft.matechai.user.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
