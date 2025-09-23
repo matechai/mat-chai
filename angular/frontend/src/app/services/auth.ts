c3c34e9abb23413728ef83d4367098bb831cf662
Ahsan Abdul Salam <ahsanbaloch899@gmail.com>
2025-09-23 19:07:39 +0200

    added file

--- angular/frontend/src/app/services/auth.ts
+++ angular/frontend/src/app/services/auth.ts
@@ -9,7 +9,6 @@ import { SignupInterface } from '../interfaces/signup-interface';
 export class Auth {
 
   private http = inject(HttpClient);
-<<<<<<< HEAD
   private apiUrl = 'http://localhost:8080/api';
 
   signup_request(request: SignupInterface): Observable<any> 
@@ -20,12 +19,5 @@ export class Auth {
   signin_request(userName: string, password: string): Observable<any>
   {
     return this.http.post(`${this.apiUrl}/auth/login`,{userName, password});
-=======
-  private apiUrl = 'http://localhost:8080/';
-
-  signup_request(request: SignupInterface): Observable<any> 
-  {
-    return this.http.post(`${this.apiUrl}api/auth/signup`, request);
->>>>>>> 70e376a (merging from main)
   }
 }
