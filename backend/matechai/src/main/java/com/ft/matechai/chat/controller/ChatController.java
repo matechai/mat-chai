079219ad8edc03a1e6ff8255a627f582ce04e305
Ahsan Abdul Salam <39459572+Ahsanbaloch@users.noreply.github.com>
2025-10-12 15:59:26 +0000

    working on websockets

---
+++ backend/matechai/src/main/java/com/ft/matechai/chat/controller/ChatController.java
@@ -0,0 +1,11 @@
+package com.ft.matechai.chat.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chat")
+public class ChatController
+{
+	
+}
