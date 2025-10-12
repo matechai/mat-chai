878fda6da9e1a2d7a67727a1be1692c2bf2fa1c3
Ahsan Abdul Salam <39459572+Ahsanbaloch@users.noreply.github.com>
2025-10-12 15:59:26 +0000

    working on websockets

--- backend/matechai/src/main/java/com/ft/matechai/config/socket/WebSocketConfig.java
+++ backend/matechai/src/main/java/com/ft/matechai/config/socket/WebSocketConfig.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.ft.matechai.config.socket;
 
 import org.springframework.context.annotation.Configuration;
@@ -27,3 +28,21 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
 		config.setUserDestinationPrefix("/user");
 	}
 }
+=======
+// package com.ft.matechai.config.socket;
+
+// import org.springframework.beans.factory.annotation.Configurable;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+// import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+// import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+// import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+// import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+// @Configuration
+// @EnableWebSocketMessageBroker
+// public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
+// {
+    
+// }
+>>>>>>> da121c5 (working on websockets)
