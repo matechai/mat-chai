2fc31f00f0879201a92d2ca7762b063f1c66e30f
Ahsan Abdul Salam <ahsalam@c1r12s1.42wolfsburg.de>
2025-10-18 11:39:17 +0200

    working on sockets

--- backend/matechai/src/main/java/com/ft/matechai/chat/controller/ChatController.java
+++ backend/matechai/src/main/java/com/ft/matechai/chat/controller/ChatController.java
@@ -1,11 +1,31 @@
 package com.ft.matechai.chat.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ft.matechai.chat.nodes.ChatMessage;
 
 @RestController
 @RequestMapping("/api/chat")
 public class ChatController
 {
-	
+	@MessageMapping("/chat.sendMessage")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage)
+	{
+		return chatMessage;
+	}
+
+	@MessageMapping("/chat.addUser")
+	@SendTo("/topic/public")
+	public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor)
+	{
+		//Add username to WebSocket session
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		return chatMessage;
+	}
 }
