80e7c8269cc2f987a507ab721c649bb99257b483
Ahsan Abdul Salam <ahsalam@c1r12s1.42wolfsburg.de>
2025-10-18 11:39:17 +0200

    working on sockets

---
+++ backend/matechai/src/main/java/com/ft/matechai/config/socket/WebSocketEventListener.java
@@ -0,0 +1,31 @@
+package com.ft.matechai.config.socket;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.ft.matechai.chat.enums.MessageType;
+import com.ft.matechai.chat.nodes.ChatMessage;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+    private final SimpMessageSendingOperations messageTemplate;
+
+    @EventListener
+    public void handleWebSocketDisconnect(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            ChatMessage chatMessage = new ChatMessage(username, MessageType.LEAVE);
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
