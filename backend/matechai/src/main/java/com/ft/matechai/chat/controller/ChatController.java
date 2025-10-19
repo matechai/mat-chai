b540141c6bad0b9f1a9a576aac5970f6d55f52ea
Ahsan Abdul Salam <ahsalam@c1r1s4.42wolfsburg.de>
2025-10-19 18:54:47 +0200

    added chat module with redis

--- backend/matechai/src/main/java/com/ft/matechai/chat/controller/ChatController.java
+++ backend/matechai/src/main/java/com/ft/matechai/chat/controller/ChatController.java
@@ -1,28 +1,60 @@
 package com.ft.matechai.chat.controller;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+=======
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+>>>>>>> f04d151 (aded chat with redis)
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ft.matechai.chat.model.ChatMessage;
+import com.ft.matechai.chat.service.ChatService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
+<<<<<<< HEAD
 // @RequiredArgsConstructor
+=======
+@RequestMapping
+>>>>>>> f04d151 (aded chat with redis)
 @Slf4j
 // @RequestMapping("/api/chat")
 public class ChatController
 {
-	private final SimpMessagingTemplate messageTemplate;
+	private final ChatService chatService;
+	
+	@Autowired
+	public ChatController(ChatService chatService)
+	{
+		this.chatService = chatService;
+	}
+
+	@PostMapping("/send")
+	public void sendMessage(@RequestBody ChatMessage message)
+	{
+		chatService.sendMessage(message);
+	}
+
+	@GetMapping("/history")
+	public List<ChatMessage> getHistory(@RequestParam String user1, @RequestParam String user2)
+	{
+		return chatService.getChatHistory(user1, user2);
+	}
+	// private final SimpMessagingTemplate messageTemplate;
 
 	// @MessageMapping("/chat.sendMessage")
 	// @SendTo("/topic/public")
@@ -40,12 +72,12 @@ public class ChatController
 			// 	// return chatMessage;
 			// }
 			
-	@PostMapping("/send")
-	public void sendMessage(@RequestBody ChatMessage chatMessage)
-	{
-		log.info("message info -> {}, {}", chatMessage.getSender(), chatMessage.getContent());
-		messageTemplate.convertAndSend("/queue" + chatMessage.getReceiver(), chatMessage);
-	}
+	// @PostMapping("/send")
+	// public void sendMessage(@RequestBody ChatMessage chatMessage)
+	// {
+	// 	log.info("message info -> {}, {}", chatMessage.getSender(), chatMessage.getContent());
+	// 	messageTemplate.convertAndSend("/queue" + chatMessage.getReceiver(), chatMessage);
+	// }
 
 
 	// @MessageMapping("/chat.addUser")
@@ -56,9 +88,12 @@ public class ChatController
 	// 	return chatMessage;
 	// }
 
+<<<<<<< HEAD
 	@MessageMapping("/chat.addUser")
 	public void addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor)
 	{
 		headerAccessor.getSessionAttributes().put("username", chatMessage.get_sender());
 	}
+=======
+>>>>>>> f04d151 (aded chat with redis)
 }
--- backend/matechai/src/main/java/com/ft/matechai/chat/service/RedisChatService.java
+++ backend/matechai/src/main/java/com/ft/matechai/chat/service/RedisChatService.java
@@ -1,6 +1,9 @@
 package com.ft.matechai.chat.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.ft.matechai.chat.model.ChatMessage;
@@ -9,15 +12,20 @@ import com.ft.matechai.chat.model.ChatMessage;
 public class RedisChatService
 {
 	@Autowired
-	// private RedisTem<String, ChatMessage> redisTemplate;
+	private RedisTemplate<String, ChatMessage> redisTemplate;
 
 	private static final String CHAT_KEY_STRING_PREFIX = "chat:";
 
 	public void saveMessage(ChatMessage message)
 	{
 		String key = CHAT_KEY_STRING_PREFIX + getChatId(message.getSender(), message.getReceiver());
-		
+		redisTemplate.opsForList().rightPush(key, message);
+	}
 
+	public List<ChatMessage> getMessage(String user1, String user2)
+	{
+		String key = CHAT_KEY_STRING_PREFIX + getChatId(user1, user2);
+		return redisTemplate.opsForList().range(key, 0, -1);
 	}
 
 	private String getChatId(String user1, String user2)
