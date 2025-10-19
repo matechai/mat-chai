4d8584058bcd48435b20ed602b7d0cb0dc3a0af0
Ahsan Abdul Salam <ahsalam@c1r1s4.42wolfsburg.de>
2025-10-19 17:49:52 +0200

    working on chat

--- backend/matechai/src/main/java/com/ft/matechai/chat/service/ChatService.java
+++ backend/matechai/src/main/java/com/ft/matechai/chat/service/ChatService.java
@@ -1,9 +1,11 @@
 package com.ft.matechai.chat.service;
 
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService
 {
-	
+	// private final SimpMessagingTemplate messagingTemplate;
+	// private final RedisChatService redsChatService;
 }
