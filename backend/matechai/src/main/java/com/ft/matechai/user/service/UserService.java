9a38a6a919fa9e0d77dc278d84fa3a21ff38bbaa
임원정 <oddish0513@gmail.com>
2025-10-24 01:43:12 +0200

    feat: create user profile PUT API #13

--- backend/matechai/src/main/java/com/ft/matechai/user/service/UserService.java
+++ backend/matechai/src/main/java/com/ft/matechai/user/service/UserService.java
@@ -18,7 +18,6 @@ import java.util.stream.Collectors;
 
 
 @Service
-@Transactional
 public class UserService {
 	private final UserRepository userRepository;
     private final GenderRepository genderRepository;
