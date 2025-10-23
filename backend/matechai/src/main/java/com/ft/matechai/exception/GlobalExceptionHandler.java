417fb85c4c00f1bb33f3b0f7ae9c19a891abfebf
임원정 <oddish0513@gmail.com>
2025-10-24 01:32:54 +0200

    refactor: code refactoring
    delete duplicate codes

--- backend/matechai/src/main/java/com/ft/matechai/exception/GlobalExceptionHandler.java
+++ backend/matechai/src/main/java/com/ft/matechai/exception/GlobalExceptionHandler.java
@@ -1,23 +1,16 @@
 package com.ft.matechai.exception;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 import java.util.Map;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(AuthExceptions.UnauthorizedException.class)
-    public ResponseEntity<String> handleUnauthorized(AuthExceptions.UnauthorizedException e) {
-        return ResponseEntity.status(401).body(e.getMessage());
-    }
-
-    @ExceptionHandler(AuthExceptions.EmailNotVerifiedException.class)
-    public ResponseEntity<String> handleEmailNotVerified(AuthExceptions.EmailNotVerifiedException e) {
-        return ResponseEntity.status(403).body(e.getMessage());
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, Object> handleEntityNotFound(EntityNotFoundException e) {
