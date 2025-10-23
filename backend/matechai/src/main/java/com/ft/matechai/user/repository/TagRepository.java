0cd453fce4658e7ac90b960aa8904e60fb1ea5eb
임원정 <oddish0513@gmail.com>
2025-10-24 01:38:42 +0200

    feat: update profile's repository #13

--- backend/matechai/src/main/java/com/ft/matechai/user/repository/TagRepository.java
+++ backend/matechai/src/main/java/com/ft/matechai/user/repository/TagRepository.java
@@ -1,11 +1,23 @@
 package com.ft.matechai.user.repository;
 
+import com.ft.matechai.exception.EntityNotFoundException;
 import com.ft.matechai.user.node.Tag;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface TagRepository extends Neo4jRepository<Tag, String> {
+import java.util.Optional;
+
+public interface TagRepository extends Neo4jRepository<Tag, Long> {
+
+    @Query("MATCH (t:Tag {name: $name}) RETURN t")
+    Optional<Tag> findByTag(@Param("name") String name);
+
+    default Tag findByTagOrThrow(String tag) {
+        return findByTag(tag)
+                .orElseThrow(() -> new EntityNotFoundException("Tag", tag));
+    }
 
     @Query("MERGE (t:Tag {name: $name}) RETURN t")
-    void saveIfNotExists(String name);
+    void saveIfNotExists(@Param("name") String name);
 }
