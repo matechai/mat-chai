d678dc8f5aed050fc61b3cfbcc3b76bd3a0c1f2a
임원정 <oddish0513@gmail.com>
2025-10-23 18:04:03 +0200

    fix: change tags id type

--- backend/matechai/src/main/java/com/ft/matechai/user/repository/TagRepository.java
+++ backend/matechai/src/main/java/com/ft/matechai/user/repository/TagRepository.java
@@ -4,7 +4,7 @@ import com.ft.matechai.user.node.Tag;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
 
-public interface TagRepository extends Neo4jRepository<Tag, Long> {
+public interface TagRepository extends Neo4jRepository<Tag, String> {
 
     @Query("MERGE (t:Tag {name: $name}) RETURN t")
     void saveIfNotExists(String name);
