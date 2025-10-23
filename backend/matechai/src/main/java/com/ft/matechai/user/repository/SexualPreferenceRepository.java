2d3503a2cde9a37a4cb21c03199efd8c6ef38231
임원정 <oddish0513@gmail.com>
2025-10-24 01:38:42 +0200

    feat: update profile's repository #13

--- backend/matechai/src/main/java/com/ft/matechai/user/repository/SexualPreferenceRepository.java
+++ backend/matechai/src/main/java/com/ft/matechai/user/repository/SexualPreferenceRepository.java
@@ -1,11 +1,24 @@
 package com.ft.matechai.user.repository;
 
+import com.ft.matechai.exception.EntityNotFoundException;
 import com.ft.matechai.user.node.SexualPreference;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface SexualPreferenceRepository extends Neo4jRepository<SexualPreference, String> {
+import java.util.Optional;
 
-    @Query("MERGE (s:SexualPreference {sexualPreference: $sexualPreference}) RETURN s")
-    void saveIfNotExists(String sexualPreference);
+
+public interface SexualPreferenceRepository extends Neo4jRepository<SexualPreference, Long> {
+
+    @Query("MATCH (s:SexualPreference {name: $name}) RETURN s")
+    Optional<SexualPreference> findBySexualPreference(@Param("name") String name);
+
+    default SexualPreference findByNameOrThrow(String name) {
+        return findBySexualPreference(name)
+                .orElseThrow(() -> new EntityNotFoundException("Sexual Preference", name));
+    }
+
+    @Query("MERGE (s:SexualPreference {name: $name}) RETURN s")
+    void saveIfNotExists(@Param("name") String name);
 }
