98f645d04bb4ab04f1b84ac86059c82c63b03f04
임원정 <oddish0513@gmail.com>
2025-10-24 19:49:07 +0200

    refcator: code refactoring
    field name change [tag -> interest]

---
+++ backend/matechai/src/main/java/com/ft/matechai/user/node/Interest.java
@@ -0,0 +1,16 @@
+package com.ft.matechai.option.node;
+
+import lombok.*;
+import org.springframework.data.neo4j.core.schema.*;
+
+@Node("Interest")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Interest {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+}
