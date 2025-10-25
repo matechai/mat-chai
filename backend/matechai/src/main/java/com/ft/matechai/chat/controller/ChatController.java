e46385103e1358c4135703e3f6054fa959d973d8
Ahsan Abdul Salam <ahsalam@c2r1s1.42wolfsburg.de>
2025-10-25 17:26:12 +0200

    added mongodb

--- backend/matechai/src/main/java/com/ft/matechai/chat/controller/ChatController.java
+++ backend/matechai/src/main/java/com/ft/matechai/chat/controller/ChatController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ft.matechai.chat.model.ChatMessage;
 import com.ft.matechai.chat.service.ChatService;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -39,49 +38,16 @@ public class ChatController
 		this.chatService = chatService;
 	}
 
-	@PostMapping("/send")
-	public void sendMessage(@RequestBody ChatMessage message)
-	{
-		chatService.sendMessage(message);
-	}
-
-	@GetMapping("/history")
-	public List<ChatMessage> getHistory(@RequestParam String user1, @RequestParam String user2)
-	{
-		return chatService.getChatHistory(user1, user2);
-	}
-	// private final SimpMessagingTemplate messageTemplate;
-
-	// @MessageMapping("/chat.sendMessage")
-	// @SendTo("/topic/public")
-	// public void sendMessage(@Payload ChatMessage chatMessage)
-	// {
-		// 	log.info("send messge form {} to {}: {}",
-		// 			chatMessage.getSender(),
-		// 			chatMessage.getReceiver(),
-		// 			chatMessage.getContent());
-		
-		// 	messageTemplate.convertAndSendToUser(
-			// 		chatMessage.getReceiver(),
-			// 		"/queue/messages",
-			// 		chatMessage);
-			// 	// return chatMessage;
-			// }
-			
 	// @PostMapping("/send")
-	// public void sendMessage(@RequestBody ChatMessage chatMessage)
+	// public void sendMessage(@RequestBody ChatMessage message)
 	// {
-	// 	log.info("message info -> {}, {}", chatMessage.getSender(), chatMessage.getContent());
-	// 	messageTemplate.convertAndSend("/queue" + chatMessage.getReceiver(), chatMessage);
+	// 	chatService.sendMessage(message);
 	// }
 
-
-	// @MessageMapping("/chat.addUser")
-	// public void addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor)
+	// @GetMapping("/history")
+	// public List<ChatMessage> getHistory(@RequestParam String user1, @RequestParam String user2)
 	// {
-	// 	//Add username to WebSocket session
-	// 	headerAccessor.getSessionAttributes().put("username", chatMessage.get_receiver());
-	// 	return chatMessage;
+	// 	return chatService.getChatHistory(user1, user2);
 	// }
 
 }
