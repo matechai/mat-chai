80e803ab275b76c5b4ea3431a5e7e1a3c607ca36
Ahsan Abdul Salam <ahsalam@c2r1s1.42wolfsburg.de>
2025-10-25 17:26:12 +0200

    added mongodb

--- backend/matechai/src/main/java/com/ft/matechai/chat/service/ChatService.java
+++ backend/matechai/src/main/java/com/ft/matechai/chat/service/ChatService.java
@@ -11,26 +11,26 @@ import com.ft.matechai.chat.model.ChatMessage;
 @Service
 public class ChatService
 {
-	private final SimpMessagingTemplate messagingTemplate;
-	private final RedisChatService redisChatService;
+	// private final SimpMessagingTemplate messagingTemplate;
+	// private final RedisChatService redisChatService;
 
-	@Autowired
-	public ChatService(SimpMessagingTemplate messagingTemplate, RedisChatService redisChatService)
-	{
-		this.messagingTemplate = messagingTemplate;
-		this.redisChatService = redisChatService;
-	}
+	// @Autowired
+	// public ChatService(SimpMessagingTemplate messagingTemplate, RedisChatService redisChatService)
+	// {
+	// 	this.messagingTemplate = messagingTemplate;
+	// 	this.redisChatService = redisChatService;
+	// }
 
-	public void sendMessage(ChatMessage message)
-	{	
-		//save to redis
-		redisChatService.saveMessage(message);
-		//send to receiver via socket
-		messagingTemplate.convertAndSend("/queue/" + message.getReceiver(), message);
-	}
+	// public void sendMessage(ChatMessage message)
+	// {	
+	// 	//save to redis
+	// 	redisChatService.saveMessage(message);
+	// 	//send to receiver via socket
+	// 	messagingTemplate.convertAndSend("/queue/" + message.getReceiver(), message);
+	// }
 
-	public List<ChatMessage> getChatHistory(String user1, String user2)
-	{
-		return redisChatService.getMessage(user1, user2);
-	}
+	// public List<ChatMessage> getChatHistory(String user1, String user2)
+	// {
+	// 	return redisChatService.getMessage(user1, user2);
+	// }
 }
