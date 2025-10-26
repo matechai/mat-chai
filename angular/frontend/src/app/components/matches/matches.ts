a2a97ac942da5b9e87d41c88dd395889f363328f
임원정 <oddish0513@gmail.com>
2025-10-26 18:27:26 +0100

    feat: empty match files create

---
+++ angular/frontend/src/app/components/matches/matches.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+	selector: 'app-matches',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './matches.html',
+	styleUrl: './matches.css'
+})
+export class Matches {
+
+}
