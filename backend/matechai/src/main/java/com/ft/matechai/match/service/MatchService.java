0227a527c08996ff4898a2ccfa47607f50414895
임원정 <oddish0513@gmail.com>
2025-10-28 17:49:56 +0100

    feat: implement matching #18

--- backend/matechai/src/main/java/com/ft/matechai/match/service/MatchService.java
+++ backend/matechai/src/main/java/com/ft/matechai/match/service/MatchService.java
@@ -1,8 +1,10 @@
 package com.ft.matechai.match.service;
 
+import com.ft.matechai.match.dto.LikeResponseDTO;
 import com.ft.matechai.user.node.User;
 import com.ft.matechai.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MatchService {
@@ -13,11 +15,29 @@ public class MatchService {
         this.userRepository = userRepository;
     }
 
-    public void like(User user, String targetUsername) {
+    @Transactional
+    public LikeResponseDTO like(User user, String targetUsername) {
 
         User targetUser = userRepository.findByUsernameOrThrow(targetUsername);
 
-        userRepository.like(user.getUsername(), targetUsername);
+        userRepository.like(user.getUsername(), targetUser.getUsername());
 
+        if (userRepository.isLikedBetween(user.getUsername(), targetUser.getUsername())) {
+
+            userRepository.match(user.getUsername(), targetUser.getUsername());
+            // todo should create chat room
+
+            return LikeResponseDTO.builder()
+                    .liked(true)
+                    .matched(true)
+                    .targetUsername(targetUser.getUsername())
+                    .build();
+        }
+
+        return LikeResponseDTO.builder()
+                .liked(true)
+                .matched(false)
+                .targetUsername(targetUser.getUsername())
+                .build();
     }
 }
