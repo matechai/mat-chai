7669d7a868d86ee60cb55fa77ade10e36a68bc4f
임원정 <oddish0513@gmail.com>
2025-10-28 17:45:39 +0100

    fix: fix query error #18

--- backend/matechai/src/main/java/com/ft/matechai/user/repository/UserRepository.java
+++ backend/matechai/src/main/java/com/ft/matechai/user/repository/UserRepository.java
@@ -75,11 +75,13 @@ public interface UserRepository extends Neo4jRepository<User, String> {
     void removeStaleInterests(@Param("username") String username,
                               @Param("newInterests") List<String> newInterests);
 
+
     // Like
     @Transactional
     @Query ("""
-                MATCH (a:User {username: $username})-[:LIKED]->(b:User {username: $targetUsername})
-                RETURN a, b
+                MATCH (a:User {username: $username})
+                MATCH (b:User {username: $targetUsername})
+                MERGE (a)-[:LIKED]->(b)
             """)
     void like(@Param("username") String username,
               @Param("targetUsername") String targetUsername);
