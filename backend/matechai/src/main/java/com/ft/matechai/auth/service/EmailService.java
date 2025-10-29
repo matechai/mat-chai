795fb5ca5073fb74fdf475174a4aeae0267b4589
임원정 <oddish0513@gmail.com>
2025-10-29 19:35:13 +0100

    chore: add email verify address log

--- backend/matechai/src/main/java/com/ft/matechai/auth/service/EmailService.java
+++ backend/matechai/src/main/java/com/ft/matechai/auth/service/EmailService.java
@@ -1,10 +1,12 @@
 package com.ft.matechai.auth.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class EmailService {
 
@@ -25,5 +27,7 @@ public class EmailService {
         message.setSubject("Verify your account");
         message.setText("Click the link to verify your account: " + link);
         mailSender.send(message);
+
+        log.info("Email verify address : " + link);
     }
 }
