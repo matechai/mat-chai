4dcbfd0ec0cbc488ce9d52618b408407e9be9dfa
Ahsan Abdul Salam <ahsanbaloch899@gmail.com>
2025-10-30 21:23:15 +0100

    removed extra files

--- backend/matechai/src/main/java/com/ft/matechai/config/socket/WebSocketConfig.java
+++ backend/matechai/src/main/java/com/ft/matechai/config/socket/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
-                .addInterceptors(jwtHandshakeInterceptor) // 👈 Add this
+                .addInterceptors(jwtHandshakeInterceptor) 
                 .setAllowedOrigins("*");
                 // .withSockJS(); 
                 //uncomment when use Frontend keep .withSockJS() —
