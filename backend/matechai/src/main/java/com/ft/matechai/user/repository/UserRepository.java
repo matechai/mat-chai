8edaeaebba0f925abb0695a71661f099bc1507f2
임원정 <oddish0513@gmail.com>
2025-11-02 23:53:56 +0100

    feat: create matching browsing #19

--- backend/matechai/src/main/java/com/ft/matechai/user/repository/UserRepository.java
+++ backend/matechai/src/main/java/com/ft/matechai/user/repository/UserRepository.java
@@ -174,6 +174,35 @@ public interface UserRepository extends Neo4jRepository<User, String> {
     boolean isBlocked(@Param("username") String username,
                   @Param("targetUsername") String targetUsername);
 
+
+    @Query("""
+                MATCH (me:User {username: $username})
+                MATCH (u:User)
+                WHERE NOT (me)-[:BLOCKED]->(u)
+                  AND NOT (u)-[:BLOCKED]->(me)
+                  AND NOT (me)-[:LIKED]->(u)
+                  AND NOT (me)-[:MATCHED]->(u)
+                  AND u.username <> me.username
+                WITH me, u
+                OPTIONAL MATCH (me)-[:HAS_GENDER]->(meGender:Gender)
+                OPTIONAL MATCH (u)-[:HAS_GENDER]->(uGender:Gender)
+                OPTIONAL MATCH (me)-[:HAS_PREFERENCE]->(mePref:SexualPreference)
+                WITH me, u, meGender, uGender, collect(mePref.name) AS myPrefs
+                
+                WHERE
+                  size(myPrefs) = 0
+                  OR
+                  (('Heterosexual' IN myPrefs) AND meGender.gender <> uGender.gender)
+                  OR
+                  (('Homosexual' IN myPrefs) AND meGender.gender = uGender.gender)
+                  OR
+                  (ANY(pref IN myPrefs WHERE pref IN ['Bisexual', 'Prefer not to say', 'Other']))
+                
+                RETURN u
+                ORDER BY u.fame DESC
+            """)
+    Page<User> getUsersForMatching(@Param("username") String username, Pageable pageable);
+
     @Query("MATCH (a:User) RETURN a")
     List<User> findAllUsers();
 }
