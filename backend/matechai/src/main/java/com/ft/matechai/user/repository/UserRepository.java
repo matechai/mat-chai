3da65b71dfdacb74d1a2c841998a2f47ab072d0c
임원정 <oddish0513@gmail.com>
2025-11-03 01:19:03 +0100

    fix: fix error & bug #19

--- backend/matechai/src/main/java/com/ft/matechai/user/repository/UserRepository.java
+++ backend/matechai/src/main/java/com/ft/matechai/user/repository/UserRepository.java
@@ -175,31 +175,53 @@ public interface UserRepository extends Neo4jRepository<User, String> {
                   @Param("targetUsername") String targetUsername);
 
 
-    @Query("""
-                MATCH (me:User {username: $username})
-                MATCH (u:User)
-                WHERE NOT (me)-[:BLOCKED]->(u)
-                  AND NOT (u)-[:BLOCKED]->(me)
-                  AND NOT (me)-[:LIKED]->(u)
-                  AND NOT (me)-[:MATCHED]->(u)
-                  AND u.username <> me.username
-                WITH me, u
-                OPTIONAL MATCH (me)-[:HAS_GENDER]->(meGender:Gender)
-                OPTIONAL MATCH (u)-[:HAS_GENDER]->(uGender:Gender)
-                OPTIONAL MATCH (me)-[:HAS_PREFERENCE]->(mePref:SexualPreference)
-                WITH me, u, meGender, uGender, collect(mePref.name) AS myPrefs
-                
-                WHERE
-                  size(myPrefs) = 0
-                  OR
-                  (('Heterosexual' IN myPrefs) AND meGender.gender <> uGender.gender)
-                  OR
-                  (('Homosexual' IN myPrefs) AND meGender.gender = uGender.gender)
-                  OR
-                  (ANY(pref IN myPrefs WHERE pref IN ['Bisexual', 'Prefer not to say', 'Other']))
-                
-                RETURN u
-                ORDER BY u.fame DESC
+    @Query(
+            value = """
+                        MATCH (me:User {username: $username})
+                        MATCH (u:User)
+                        WHERE NOT (me)-[:BLOCKED]->(u)
+                          AND NOT (u)-[:BLOCKED]->(me)
+                          AND NOT (me)-[:LIKED]->(u)
+                          AND NOT (me)-[:MATCHED]->(u)
+                          AND u.username <> me.username
+                        WITH me, u
+                        OPTIONAL MATCH (me)-[:HAS_GENDER]->(meGender:Gender)
+                        OPTIONAL MATCH (u)-[:HAS_GENDER]->(uGender:Gender)
+                        OPTIONAL MATCH (me)-[:HAS_PREFERENCE]->(mePref:SexualPreference)
+                        WITH me, u, meGender, uGender, collect(mePref.name) AS myPrefs
+                        WHERE
+                          size(myPrefs) = 0
+                          OR
+                          (('Heterosexual' IN myPrefs) AND meGender.gender <> uGender.gender)
+                          OR
+                          (('Homosexual' IN myPrefs) AND meGender.gender = uGender.gender)
+                          OR
+                          (ANY(pref IN myPrefs WHERE pref IN ['Bisexual', 'Prefer not to say', 'Other']))
+                        RETURN u
+                        ORDER BY u.fame DESC
+                    """,
+            countQuery = """
+                            MATCH (me:User {username: $username})
+                            MATCH (u:User)
+                            WHERE NOT (me)-[:BLOCKED]->(u)
+                              AND NOT (u)-[:BLOCKED]->(me)
+                              AND NOT (me)-[:LIKED]->(u)
+                              AND NOT (me)-[:MATCHED]->(u)
+                              AND u.username <> me.username
+                            WITH me, u
+                            OPTIONAL MATCH (me)-[:HAS_GENDER]->(meGender:Gender)
+                            OPTIONAL MATCH (u)-[:HAS_GENDER]->(uGender:Gender)
+                            OPTIONAL MATCH (me)-[:HAS_PREFERENCE]->(mePref:SexualPreference)
+                            WITH me, u, meGender, uGender, collect(mePref.name) AS myPrefs
+                            WHERE
+                              size(myPrefs) = 0
+                              OR
+                              (('Heterosexual' IN myPrefs) AND meGender.gender <> uGender.gender)
+                              OR
+                              (('Homosexual' IN myPrefs) AND meGender.gender = uGender.gender)
+                              OR
+                              (ANY(pref IN myPrefs WHERE pref IN ['Bisexual', 'Prefer not to say', 'Other']))
+                            RETURN count(u)
             """)
     Page<User> getUsersForMatching(@Param("username") String username, Pageable pageable);
 
