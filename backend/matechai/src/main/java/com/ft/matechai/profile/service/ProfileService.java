23853ae6616644336b892f866bbde5f9ed588d7f
임원정 <oddish0513@gmail.com>
2025-11-05 19:59:50 +0100

    fix: add transactional annotation

--- backend/matechai/src/main/java/com/ft/matechai/profile/service/ProfileService.java
+++ backend/matechai/src/main/java/com/ft/matechai/profile/service/ProfileService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -33,6 +34,7 @@ public class ProfileService {
                 ));
     }
 
+    @Transactional
     public void updateLocation(String username, LocationDTO location) {
 
         User user = userRepository.findByUsernameOrThrow(username);
