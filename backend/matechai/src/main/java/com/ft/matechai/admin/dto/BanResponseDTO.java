67290308247449e3bbff63d144a0e380fd3353dd
임원정 <oddish0513@gmail.com>
2025-11-05 22:04:37 +0100

    feat: implement ban api #23

---
+++ backend/matechai/src/main/java/com/ft/matechai/admin/dto/BanResponseDTO.java
@@ -0,0 +1,10 @@
+package com.ft.matechai.admin.dto;
+
+import lombok.Data;
+
+@Data
+public class BanResponseDTO {
+
+    private String username;
+    private boolean isBanned;
+}
--- backend/matechai/src/main/java/com/ft/matechai/admin/service/AdminService.java
+++ backend/matechai/src/main/java/com/ft/matechai/admin/service/AdminService.java
@@ -1,8 +1,11 @@
 package com.ft.matechai.admin.service;
 
+import com.ft.matechai.admin.dto.BanResponseDTO;
 import com.ft.matechai.admin.dto.ReportResponseDTO;
+import com.ft.matechai.user.node.User;
 import com.ft.matechai.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 import java.util.List;
@@ -22,5 +25,12 @@ public class AdminService {
     public List<ReportResponseDTO> getReports() {
 
         return userRepository.getReportedUsers();
+    }
+
+    @Transactional
+    public BanResponseDTO banUser(String username) {
+
+        User user = userRepository.findByUsernameOrThrow(username);
+        return userRepository.ban(user.getUsername());
     }
 }
