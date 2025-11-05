c27c60fef1a848aefb2487476728541a599c3a7b
임원정 <oddish0513@gmail.com>
2025-11-05 19:21:19 +0100

    feat: add graphQL DateTime scalar #21

--- backend/matechai/src/main/java/com/ft/matechai/config/GraphQLConfig.java
+++ backend/matechai/src/main/java/com/ft/matechai/config/GraphQLConfig.java
@@ -1,10 +1,18 @@
 package com.ft.matechai.config;
 
+import graphql.scalars.ExtendedScalars;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 
 @Configuration
 @DependsOn("filterChain") // Ensure Security is initialized first
 public class GraphQLConfig {
-    // Define custom schemas and data fetchers if necessary
+
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        return wiringBuilder -> wiringBuilder
+                .scalar(ExtendedScalars.DateTime);
+    }
 }
